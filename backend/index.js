0f14eba9d4065bda22f7968112ab0c47047cc16f
Saksham Solanki <saksham.solanki@gmail.com>
2024-04-13 04:02:06 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import config from './config.js';
 import { userRouter } from './Routes/UserRoute.js';
 import { loanAgreementRouter } from './Routes/LoanAgreementsRoute.js';
+import { loanRequestsRouter } from './Routes/LoanRequestsRoute.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/users', userRouter);
 app.use('/api/loanAgreements' , loanAgreementRouter);
+app.use('/api/loanRequests' , loanRequestsRouter);
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`),
